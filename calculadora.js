f7ab4896174828cdc3f838f8edd8aff9d50474fe
ScuderiFernando <scuderi.f@hotmail.com>
2021-05-20 21:53:09 -0300

    tp calculadora

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+let sumar = require("./sumar")
+let restar = require("./restar")
+let multiplicar = require("./multiplicar")
+let dividir = require("./dividir")
+
+
+
+console.log(sumar(2,2))
+
+console.log(restar(8,2))
+
+console.log(multiplicar(3,3))
+
+console.log(multiplicar(0,3))
+
+console.log(dividir(15,3))
+
+console.log(dividir(15,0))
